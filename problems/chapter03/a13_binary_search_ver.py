ca53c87baabd14cb6e395601e4239e9e1d51bf82
TsubasaKanemitsu <kanemitsu@TsubasaKanemitsunoMacBook-Air.local>
2024-07-25 08:33:53 -0700

    add: a13 resolutinon binary search version

---
+++ problems/chapter03/a13_binary_search_ver.py
@@ -0,0 +1,24 @@
+from typing import List
+
+N, K = list(map(int, input().split()))
+A = list(map(int, input().split()))
+
+def search(a: int, l: int, N: int, K: int) -> int:
+    r = len(A)
+
+    while r - l > 1:
+        m = (l + r) // 2
+        if A[m] <= a + K:
+            l = m
+        else:
+            r = m
+    return l
+
+# 開始位置の決定
+cnt = 0
+for i in range(N):
+    # A[t] <= A[i] + KとなるA[t]を求める
+    # 二分探索（最小値の最大化）
+    t = search(a=A[i], l=i, N=N, K=K)
+    cnt += t - i
+print(cnt)
