506ef41fb499978c5826080ecf093feda838601a
TsubasaKanemitsu <kanemitsu@TsubasaKanemitsunoMacBook-Air.local>
2024-08-01 07:42:10 -0700

    fix: is prime

--- problems/chapter05/a26.py
+++ problems/chapter05/a26.py
@@ -4,15 +4,10 @@ Q = int(input())
 X = [int(input()) for _ in range(Q)]
 
 def is_prime(x: int) -> bool:
-    i = 2
-    flg = True
-    while i <= math.sqrt(x):
+    for i in range(2, x ** 0.5 + 1):
         if x % i == 0:
-            flg = False            
-            break
-        i += 1
-
-    return flg
+            return False
+    return True
 
 for x in X:
     flg = is_prime(x)
