c37e06638558ce1064639bbd7e3dc67765ecac32
TsubasaKanemitsu <kanemitsu@TsubasaKanemitsunoMacBook-Air.local>
2024-08-01 07:42:50 -0700

    fix: is prime

--- problems/chapter05/a26.py
+++ problems/chapter05/a26.py
@@ -4,7 +4,7 @@ Q = int(input())
 X = [int(input()) for _ in range(Q)]
 
 def is_prime(x: int) -> bool:
-    for i in range(2, x ** 0.5 + 1):
+    for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             return False
     return True
